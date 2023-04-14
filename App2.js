37712ead71ca8ec05d81eb058c4d75fb50a9eb45
lucas salvatore <lucaseli2979@gmail.com>
2023-04-14 13:13:04 +0000

    first commit

---
+++ App2.js
@@ -0,0 +1,56 @@
+// JavaScript Beginner's Course.
+
+// VARIABLES;
+
+let age = 25;
+console.log(age);
+
+const password = 1234;
+password = 4321;
+console.log(password);
+
+// Data git Types
+// Strings;
+const name = "JavaSCript";
+console.log(name);
+
+// Undefined
+console.log(name);
+
+// Objects(non-primitive data/ also known as object Literals)
+
+const user1 = {
+  FirstName: "Lumen",
+  MiddleName: "Creative",
+  LastName: "Studios",
+  age: "30",
+};
+
+console.log(user1.FirstName);
+console.log(user1.MiddleName);
+console.log(user1.LastName);
+
+const Inputs = [2, 4, 6, 8, 10];
+console.log(Inputs[1]);
+
+// Operators(Arithmetic);
+let name1 = "Lumen";
+let age1 = "30";
+
+console.log(name + age1);
+console.log(name == age1);
+console.log(name !== age1);
+console.log(name === age1);
+console.log("Lumen " + "studios");
+
+// Conversions(Implicit / Explicit);
+console.log(true + "4");
+console.log("2" - "3");
+console.log(0.1 + 0.2 === 0.3);
+console.log("2" * "4");
+console.log("3" * "Twitter");
+console.log("Ama" - "Kofi");
+
+console.log("8" * 4);
+console.log("5" / true);
+console.log("5" + 5);
